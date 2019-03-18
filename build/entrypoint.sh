1aeb68a235129ca0227fb21703136f6566dbc25c
darkadept <darkadept@durbn.net>
2019-03-18 09:38:39 -0500

    checking in leftovers

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -15,6 +15,18 @@ if [ ! -d "${DATA_PATH}/data" ]; then
 	chown -R nginx:nginx "${DATA_PATH}/data"
 fi
 
+if [ ! -d "${DATA_PATH}/plugins" ]; then
+	mkdir ${DATA_PATH}/plugins
+	cp -a /original/plugins/* ${DATA_PATH}/plugins
+	chown -R nginx:nginx "${DATA_PATH}/plugins"
+fi
+
+if [ ! -d "${DATA_PATH}/tpl" ]; then
+	mkdir ${DATA_PATH}/tpl
+	cp -a /original/tpl/* ${DATA_PATH}/tpl
+	chown -R nginx:nginx "${DATA_PATH}/tpl"
+fi
+
 if [ ! -f "${WEB_PATH}/conf" ]; then
 	ln -s ${DATA_PATH}/conf ${WEB_PATH}/
 fi
@@ -23,8 +35,19 @@ if [ ! -f "${WEB_PATH}/data" ]; then
 	ln -s ${DATA_PATH}/data ${WEB_PATH}/
 fi
 
+if [ ! -f "${WEB_PATH}/plugins" ]; then
+	ln -s ${DATA_PATH}/plugins ${WEB_PATH}/lib/
+fi
+
+if [ ! -f "${WEB_PATH}/tpl" ]; then
+	ln -s ${DATA_PATH}/tpl ${WEB_PATH}/lib/
+fi
+
 chown -R nginx:nginx ${DATA_PATH}
 chmod -R a+rw ${DATA_PATH}/data
 chmod -R 644 ${DATA_PATH}/conf/*
+chmod go+rw ${DATA_PATH}/conf
+chmod -R a+rw ${DATA_PATH}/plugins
+chmod -R a+rw ${DATA_PATH}/tpl
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
